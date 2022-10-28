6af859e929db54b5424c6545152fcc5c199defe0
Elliot Beck <elliot.beck@hotmail.com>
2022-10-28 16:23:24 +0200

    add enironment.yml file for conda env

--- homework/week2/csv-to-scatter-plot.py
+++ homework/week2/csv-to-scatter-plot.py
@@ -2,17 +2,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Read a CSV file "data.csv" shared to the Docker container from the Docker host.
-# The header line is skipped, and typically contains a description of the columns:
-# [ x-coordinate, y-coordinate, colour, size ]
-# Each row of the CSV file (other than the header) defines one point to plot.
-the_data = np.genfromtxt('data/coding-environment-exercise.csv', delimiter=';',skip_header=1)
+# Read a CSV file "data.csv" 
+the_data = np.genfromtxt('data/coding-environment-exercise.csv', 
+                         delimiter=';',skip_header=1)
 transposed_data = the_data.transpose()
 
+# extract data for plot
 points_x = transposed_data[0]
 points_y = transposed_data[1]
-
-# This code is not at all robust, but skipping error handling makes it more brief.
 
 # Define the scatter plot
 f = plt.figure()
