c92bf5d5f282ee9d81d447fdad6104f33081a8b5
Elliot Beck <elliot.beck@hotmail.com>
2022-12-09 17:58:35 +0100

    introduce the shiny app

--- src/models/analysis.R
+++ src/models/analysis.R
@@ -13,8 +13,19 @@ avg_prc <- colMeans(group_r) * 100
 cor <- cor(group_r)
 cor2 <- cor(pce_data_growth)
 cor2_12m <- cor(pce_data_growth_yoy)
-
 results_cor_avg <-  rbind(cor, avg_prc)
+correlations <- list(
+    avg_prc,
+    cor,
+    cor2,
+    cor2_12m,
+    results_cor_avg)
+
+# save the correlation output
+save(correlations,
+    file = "src/visualization/correlations.RData")
+
+# fit the models
 model_all <- summary(lm(hicp ~ cmd_all - 1, data = group_r))
 model_agr <- summary(lm(hicp ~ cmd_agr - 1, data = group_r))
 model_food <- summary(lm(hicp ~ cmd_food - 1, data = group_r))
@@ -28,6 +39,8 @@ results_model <- rbind(model_all$coefficients,
       model_gold$coefficients,
       model_met$coefficients,
       model_oil$coefficients)
+# save the model output
+save(results_model, file = "src/visualization/results_models_lm.RData")
 
 # Rolling correlations HICP vs commodities
 a <- nrow(group_c)-floor(nrow(group.c) / 12) * 12
@@ -49,3 +62,6 @@ rownames(roll_2) <- colnames(group_r)
 roll_2 <- roll_2[2:7, ]
 time <- c(2004:2022)
 colnames(roll_2) <- time
+
+# save the rolling correlation output
+save(roll_2, file = "src/visualization/roll_correlations.RData")
